9009d7ad46c4c1d51627330cfd4a1e2738c1199e
onggq37 <84619503+onggq37@users.noreply.github.com>
2021-10-10 16:26:11 +0800

    connect to db

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+//Connect DB
+connectDB();
 
 app.get('/', (req, res) => {
     res.send(`API running`);
