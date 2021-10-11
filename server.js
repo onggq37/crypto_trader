89fffde481ffa32416cf0ffd8ce9d6b6665fbb1b
onggq37 <84619503+onggq37@users.noreply.github.com>
2021-10-11 23:02:00 +0800

    User schema and user route and auth route added

--- server.js
+++ server.js
@@ -11,4 +11,7 @@ app.get('/', (req, res) => {
     res.send(`API running`);
 })
 
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+
 app.listen(PORT, () => console.log(`Server on port ${PORT}`))
