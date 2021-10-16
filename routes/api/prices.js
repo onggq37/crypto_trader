5c6049635479091549d95129f20c86adf227008e
onggq37 <84619503+onggq37@users.noreply.github.com>
2021-10-16 12:58:05 +0800

    git push price page

--- routes/api/prices.js
+++ routes/api/prices.js
@@ -9,27 +9,32 @@ router.get("/", async (req, res) => {
   try {
     const allCoinsApi = await CoinGeckoClient.coins.all();
     // console.log(allCoinsApi);
-    const listOfCoins = []
+    const listOfCoins = [];
     // console.log(allCoinsApi.data[0].market_data);
-    for ( const obj of allCoinsApi.data ) {
-        const coinData = {
-            symbol: obj.symbol,
-            name: obj.name,
-            image: obj.image.small,
-            price: obj.market_data.current_price.usd,
-            priceChange24Hr: obj.market_data.price_change_24h,
-            percentPriceChange24Hr: obj.market_data.price_change_percentage_24h,
-            marketCap: obj.market_data.market_cap.usd,
-        }
+    for (const obj of allCoinsApi.data) {
+      const coinData = {
+        symbol: obj.symbol,
+        name: obj.name,
+        image: obj.image.small,
+        price: obj.market_data.current_price.usd,
+        priceChange24Hr: obj.market_data.price_change_24h,
+        percentPriceChange24Hr: obj.market_data.price_change_percentage_24h,
+        marketCap: obj.market_data.market_cap.usd,
+      };
 
-        console.log(coinData)
+      listOfCoins.push(coinData);
     }
+    //For now just send list
+    res.send(listOfCoins);
   } catch (e) {
     console.error(e.message);
     res.status(500).send("Server error");
   }
+});
 
-  res.send("Price page");
+router.get("/:id", async (req, res) => {
+    
+    res.send("Show Price");
 });
 
 module.exports = router;
