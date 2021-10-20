b1667e2f9c510068d48c93397d461297281fef69
gyq765 <kenji.gohyiqian@gmail.com>
2021-10-20 18:16:39 +0800

    update transfer funds page

--- client/src/pages/TransferPage.jsx
+++ client/src/pages/TransferPage.jsx
@@ -1,19 +1,42 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
+import { Container, Col, Row, Card, Tabs, Tab } from "react-bootstrap";
+import Deposit from "./Deposit";
+import Withdrawal from "./Withdrawal";
 import ThemeContext from "../ThemeContext";
+import TransferHistory from "./TransferHistory";
 
-const AcademyPage = () => {
+const TransferPage = () => {
+  const [key, setKey] = useState("balances");
   const { theme } = useContext(ThemeContext);
   return (
-    <div className={`Login ${theme}`}>
-      <h1>Transfer History</h1>
-      <img
-        id="logo"
-        src="images/under-construction.png"
-        alt="logo"
-        height="300px"
-      />
-    </div>
+    <Container>
+      <Row xs={1} md={1}>
+        <Col>
+          <Card className={`walletCard ${theme}`}>
+            <Card.Img id="icon" />
+            <Card.Body>
+              <h1>Funds Transfer</h1>
+              <Tabs
+                id="controlled-tab-example"
+                activeKey={key}
+                onSelect={(k) => setKey(k)}
+                className="mb-3"
+              >
+                <Tab eventKey="balances" title="Deposit">
+                  <Deposit />
+                </Tab>
+                <Tab eventKey="transaction" title="Withdrawal">
+                  <Withdrawal />
+                </Tab>
+              </Tabs>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <br />
+      <TransferHistory />
+    </Container>
   );
 };
 
-export default AcademyPage;
+export default TransferPage;
--- client/src/pages/WalletPage.jsx
+++ client/src/pages/WalletPage.jsx
@@ -1,39 +1,41 @@
 import React, { useState, useContext } from "react";
 import { Container, Col, Row, Card, Tabs, Tab } from "react-bootstrap";
 import { withRouter } from "react-router";
-import BalancePage from "./BalancePage";
 import TransactionPage from "./TransactionPage";
 import TransferPage from "./TransferPage";
+import BalancePage from "./BalancePage";
 import ThemeContext from "../ThemeContext";
+import { GiMoneyStack } from "react-icons/gi";
 
-const WalletPage = () => {
+const WalletPage = ({ status, setStatus }) => {
   const [key, setKey] = useState("balances");
   const { theme } = useContext(ThemeContext);
 
   return (
     <div className={`walletPage ${theme}`}>
-      <h1>Your Wallet</h1>
+      <h1>
+        Your Wallet <GiMoneyStack />
+      </h1>
       <p> Notional values as of {new Date().toLocaleString()}</p>
       <Container>
         <Row xs={1} md={1}>
           <Col>
             <Card className={`walletCard ${theme}`}>
-              <Card.Img id="icon" src="" />
+              <Card.Img id="icon" />
               <Card.Body>
-                <Card.Title>Account Balances</Card.Title>
                 <Tabs
                   id="controlled-tab-example"
                   activeKey={key}
                   onSelect={(k) => setKey(k)}
                   className="mb-3"
                 >
-                  <Tab eventKey="balances" title="Balances">
+                  <Tab eventKey="balances" title="Account Balances">
                     <BalancePage />
                   </Tab>
                   <Tab eventKey="transaction" title="Transaction History">
                     <TransactionPage />
                   </Tab>
-                  <Tab eventKey="transfer" title="Transfer History">
+                  <Tab eventKey="transfer" title="Funds Transfer">
                     <TransferPage />
                   </Tab>
                 </Tabs>
