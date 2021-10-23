ad69337a810727da4865a85c8219fa2a4edefde9
onggq37 <84619503+onggq37@users.noreply.github.com>
2021-10-23 09:18:23 +0800

    prep for heroku

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const path = require("path");
 
 //Connect DB
 connectDB();
@@ -12,10 +13,6 @@ connectDB();
 //Init middleware
 app.use(express.json({ extended: false }));
 app.use("/images", express.static(path.join(__dirname, "/images")));
-
-app.get("/", (req, res) => {
-  res.send(`API running`);
-});
 
 // multer for blogpost
 const storage = multer.diskStorage({
@@ -37,5 +34,14 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/prices", require("./routes/api/prices"));
 app.use("/api/wallet", require("./routes/api/wallet"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+//server static asset in production
+if (process.env.NODE_ENV === "production") {
+  //set static
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server on port ${PORT}`));
